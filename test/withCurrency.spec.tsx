d8949bcd3f52bcc92ca65772cd49e228f9e1284e
Jakub Biesiada <biesiadajakub@icloud.com>
2020-02-22 18:52:12 +0100

    Update

--- test/withCurrency.spec.tsx
+++ test/withCurrency.spec.tsx
@@ -1,18 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from '@testing-library/react';
 
 import { withCurrency } from '../src';
-
-const App = ({ convert }: any) => {
-  const value = convert(200, {
-    from: 'USD',
-    to: 'GBP',
-    base: 'EUR',
-    rates
-  });
-
-  return <p>{value}</p>;
-};
 
 const rates = {
   GBP: 0.92,
@@ -21,11 +10,51 @@ const rates = {
   USD: 1.12
 };
 
-const AppHOC = withCurrency(App);
+describe('withCurrency usage with functional component', () => {
+  const FunctionalComponent = ({ convert }: any) => {
+    const value = convert(200, {
+      from: 'USD',
+      to: 'GBP',
+      base: 'EUR',
+      rates
+    });
 
-describe('withCurrency', () => {
-  it('should', () => {
-    const { container } = render(<AppHOC />);
+    return <p>{value}</p>;
+  };
+
+  const FunctionalComponentHOC = withCurrency(FunctionalComponent);
+
+  it('should render converted value', () => {
+    const { container } = render(<FunctionalComponentHOC />);
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <p>
+        164.28571428571428
+      </p>
+    `);
+  });
+});
+
+describe('withCurrency usage with class component', () => {
+  class ClassComponent extends Component<any> {
+    render() {
+      const { convert } = this.props;
+
+      const value = convert(200, {
+        from: 'USD',
+        to: 'GBP',
+        base: 'EUR',
+        rates
+      });
+
+      return <p>{value}</p>;
+    }
+  }
+
+  const ClassComponentHOC = withCurrency(ClassComponent);
+
+  it('should render converted value', () => {
+    const { container } = render(<ClassComponentHOC />);
 
     expect(container.firstChild).toMatchInlineSnapshot(`
       <p>
