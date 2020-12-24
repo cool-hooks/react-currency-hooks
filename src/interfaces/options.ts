e5831fa53fa9919011a86067d138657c3af79751
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-24 12:26:02 +0100

    chore: update

--- src/interfaces/options.ts
+++
@@ -1,9 +0,0 @@
-import type { Rates } from '../types/rates';
-
-export interface Options {
-  readonly from: string;
-  readonly to: string | string[];
-  readonly base: string;
-  readonly rates: Readonly<Rates>;
-  readonly keepPrecision?: boolean;
-}
