4facb6275f9c97d2f8557b48058153ed6db12e7b
Jakub Biesiada <kbba@op.pl>
2021-02-20 21:55:26 +0100

    chore: update (#34)

--- src/useCurrency.ts
+++ src/useCurrency.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import { hasKey } from './helpers/hasKey';
 
@@ -13,29 +13,37 @@ export const useCurrency = (amount: number, options: Options) => {
     to instanceof Array ? {} : undefined
   );
 
-  const getRate = (to: string) => {
-    if (from === base && hasKey(rates, to)) {
-      return rates[to];
-    }
+  const getRate = useCallback(
+    (to: string) => {
+      if (from === base && hasKey(rates, to)) {
+        return rates[to];
+      }
 
-    if (to === base && hasKey(rates, from)) {
-      return 1 / rates[from];
-    }
+      if (to === base && hasKey(rates, from)) {
+        return 1 / rates[from];
+      }
 
-    if (hasKey(rates, from) && hasKey(rates, to)) {
-      return rates[to] * (1 / rates[from]);
-    }
+      if (hasKey(rates, from) && hasKey(rates, to)) {
+        return rates[to] * (1 / rates[from]);
+      }
 
-    throw new Error(
-      '`rates` object does not contain either `from` or `to` currency!'
-    );
-  };
+      throw new Error(
+        '`rates` object does not contain either `from` or `to` currency!'
+      );
+    },
+    [base, from, rates]
+  );
 
-  const convert = (to: string) => {
-    const convertedValue = amount * 100 * getRate(to);
+  const convert = useCallback(
+    (to: string) => {
+      const convertedValue = amount * 100 * getRate(to);
 
-    return (keepPrecision ? convertedValue : Math.round(convertedValue)) / 100;
-  };
+      return (
+        (keepPrecision ? convertedValue : Math.round(convertedValue)) / 100
+      );
+    },
+    [amount, getRate, keepPrecision]
+  );
 
   useEffect(() => {
     if (to instanceof Array) {
